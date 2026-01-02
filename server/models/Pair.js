0bad11f871f94f59afadd6d1153723a328567923
Mehedi Hasan Ariyan <thebotolbaba@gmail.com>
2026-01-02 12:38:02 +0600

    Cleanup: Remove frontend files from backend repo and restore folder structure

--- server/models/Pair.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const pairSchema = new mongoose.Schema({
-    symbol: { type: String, required: true, unique: true }, // e.g. BTCUSDT
-    is_active: { type: Boolean, default: true }
-});
-
-module.exports = mongoose.model('Pair', pairSchema);
